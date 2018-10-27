037d793483b251fe3723d1c8e984fef50914b1d9
lianna-blanca <lianna.blanca@gmail.com>
2018-10-27 15:30:12 +1300

    Add placeholder handle functions and clear button

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -6,34 +6,46 @@ constructor(props) {
   this.state = {value: ""}
 
   this.handleChange = this.handleChange.bind(this)
-  this.handleSubmit = this.handleSubmit.bind(this)
+  this.handleSubmittedName = this.handleSubmittedName.bind(this)
+  this.handleSubmitNoName = this.handleSubmitNoName.bind(this)
+  this.clearInput = this.clearInput.bind(this)
 }
 
 handleChange(ev) {
-  this.setState({value: event.target.value})
+  this.setState({value: ev.target.value})
 }
 
-handleSubmit(ev) {
+handleSubmittedName(ev) {
   ev.preventDefault();
   if (this.state.value) {
     console.log(this.state.value)
   }
   else {
-    console.log("no name")
+    this.handleSubmitNoName(ev)
   }
+}
+
+handleSubmitNoName(ev) {
+  ev.preventDefault();
+  console.log("no name")
+}
+
+clearInput() {
+  this.setState({value: ""})
+  console.log("cleared")
 }
 
 render() {
 return (
 <div>
-  <form onSubmit={this.handleSubmit}>
+  <form onSubmit={this.handleSubmittedName}>
     <div className="form-group">
       <button type="submit" value="submit" className="btn btn-danger btn-lg">Generate Conspiracy Theory</button>
     </div>
     <div className="form-group">
       <label className="control-label" htmlFor="pwd">
         <h4>Enter your name for a customised conspiracy theory:</h4>
-        <input type="text" value={this.state.value} onChange={this.handleChange} id="name" name="noun" />
+        <input type="text" value={this.state.value} onChange={this.handleChange} id="name" name="noun" /><button type="reset" onClick={() => {this.clearInput()}}>Clear</button>
       </label>
     </div>
   </form>
