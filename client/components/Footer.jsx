d516fb9dc501cc4135af07b35dd38c4474b30ce1
lianna-blanca <lianna.blanca@gmail.com>
2018-11-22 19:22:28 +1300

    Restore some classes

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -2,8 +2,12 @@ import React from "react"
 
 const Footer = () => {
 return (
-  <div>
-    <p>By TEAM CONSPIRACY THEORY: <a href="https://github.com/lianna-blanca">Lianna</a>, <a href="https://github.com/mattritani">Matt</a>, <a href="https://github.com/Nepcam">Cameron</a> and <a href="https://github.com/lukechadwick">Luke</a>
+  <div className="footer">
+    <p>
+      By TEAM CONSPIRACY THEORY: <a href="https://github.com/lianna-blanca">Lianna</a>, <a href="https://github.com/mattritani">Matt</a>, <a href="https://github.com/Nepcam">Cameron</a> and <a href="https://github.com/lukechadwick">Luke</a>
+    </p>
+    <p>
+      This version by <a href="https://github.com/lianna-blanca">Lianna</a>.
     </p>
   </div>
 )
