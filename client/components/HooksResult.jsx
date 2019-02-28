3c5acdd877a1a42b3fb7498d51ffa8228f8ca01f
lianna-blanca <lianna.blanca@gmail.com>
2019-03-01 09:31:29 +1300

    Result function working sans submittedName, will restore to one component

--- client/components/HooksResult.jsx
+++ client/components/HooksResult.jsx
@@ -1,54 +1,46 @@
 import React, { useState } from "react"
 let data = require("./theoryText.json");
 
-// // rebulid WIP
+// rebulid WIP, still need to get submittedName 
+
+function randomy(arr) {
+  return arr[Math.floor((Math.random() * (arr.length - 1)))]
+}
+
+function generateConspiracyHooked() {
+  let introText = this.randomy(data.intros)
+  let nounPhrase = this.randomy(data.nounPhrases)
+  let verbPhrase = this.randomy(data.verbPhrases)
+  let transitivePhrase = this.randomy(data.transitivePhrases)
+  let obj = this.randomy(data.objects)
+  let sentence = this.randomy(data.sentences)
+  let theory = ""
+
+  if (submittedName) {
+    theory = submittedName + verbPhrase
+  }
+  else {
+    let num = Math.random()
+    if (num >= 0.3) {
+      theory = nounPhrase + verbPhrase
+    }
+    else if (num < 0.3 && num <= 0.7) {
+      theory = transitivePhrase + obj
+    }
+  else theory = sentence
+  }
 
 
-  // this.buildTheory = this.buildTheory.bind(this)
-  // this.randomy = this.randomy.bind(this)
-
-// randomy(arr) {
-//   return arr[Math.floor((Math.random() * (arr.length - 1)))]
-// }
-
-
-// buildTheory(submittedName) {
-//   let intro = this.randomy(data.intros)
-// 	let nounPhrase = this.randomy(data.nounPhrases)
-// 	let verbPhrase = this.randomy(data.verbPhrases)
-// 	let transitivePhrase = this.randomy(data.transitivePhrases)
-// 	let obj = this.randomy(data.objects)
-// 	let sentence = this.randomy(data.sentences)
-
-// let theory = ""
-
-// if (submittedName) {
-//   theory = submittedName + verbPhrase
-// }
-// else {
-//   let num = Math.random()
-//   if (num >= 0.3) {
-//     theory = nounPhrase + verbPhrase
-//   }
-//   else if (num < 0.3 && num <= 0.7) {
-//     theory = transitivePhrase + obj
-//   }
-//   else theory = sentence
-// }
-
-// document.getElementById("intro").innerHTML = intro
-// document.getElementById("the-conspiracy").innerHTML = theory
-// }
-
-
+return (
+  <div className="conspiracy-result">
+    <p id="intro">{introText}</p>
+    <p id="the-conspiracy">{theory}</p>
+  </div>
+)
+}
 
 const HooksResult = () => {
-// return (
-  // <div className="conspiracy-result">
-  //   <p id="intro"></p>
-  //   <p id="the-conspiracy"></p>
-  // </div>
-// )
+  return {generateConspiracyHooked()}
 }
 
 export default HooksResult
